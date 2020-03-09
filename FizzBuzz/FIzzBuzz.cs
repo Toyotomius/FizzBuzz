6c31a5566a981acc97808f6a6d10fc8068191132
Toyotomius <Toyotomius@gmail.com>
2020-03-08 22:31:58 -0300

    Added null checks and high < low sanity check and handling.

--- FizzBuzz/FIzzBuzz.cs
+++ FizzBuzz/FIzzBuzz.cs
@@ -1,4 +1,6 @@
-﻿namespace FizzBuzz
+﻿using System;
+
+namespace FizzBuzz
 {
     public class FizzBuzz : IFindValuesInRangeWithPredicates<int, string>
     {
@@ -11,11 +13,15 @@
 
         public void Execute(int low, int high, Predicates<int, string> predicates)
         {
+            if (high < low) { throw new ArgumentException("High value of range is less than low value."); }
+            if (predicates is null) { throw new ArgumentNullException(nameof(predicates)); }
+
             for (var i = low; i <= high; i++)
             {
                 var fizzBuzz = "";
                 foreach (var predicate in predicates.GetPredicates())
                 {
+                    if (predicate is null) { continue; }
                     fizzBuzz += predicate(i);
                 }
 
